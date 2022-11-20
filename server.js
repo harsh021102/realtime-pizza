be2dfd50e9387e838eb282e5f9990332e4af979a
harsh021102 <harsh021102@gmail.com>
2022-11-21 01:24:20 +0530

    Final

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const PORT = process.env.PORT||3000
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 
-// Assets
+// database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("Database connected...");
+});
+
 app.use(express.static('public'))
 
 
@@ -15,18 +25,8 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req,res)=>{
-    res.render('home')
-})
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+require('./routes/web')(app)
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
